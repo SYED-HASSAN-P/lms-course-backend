6b16d010a2d4edbf9735a765f9cdf8131d013cf3
Syed Mohammad Hassan <trollfever.69@gmail.com>
2025-08-15 10:55:46 +0530

    update

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -8,4 +7,6 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET
 });
 
-module.exports = cloudinary;
+cloudinary.api.ping()
+  .then(() => console.log('✅ Cloudinary connected'))
+  .catch(err => console.error('❌ Cloudinary error:', err.message));
